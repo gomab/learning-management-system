cb07513d6fb52f4c6bed1312b6ee4e7c2332861b
Gomab <egoiomab.com>
2024-04-12 06:01:59 -0700

    S4 11. Login Serializer in Django

--- userauths/admin.py
+++ userauths/admin.py
@@ -4,8 +4,8 @@ from userauths.models import User, Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'full_name', 'date']
     
-    admin.site.register(User)
-    admin.site.register(Profile)
+admin.site.register(User)
+admin.site.register(Profile, ProfileAdmin)
     
     
     
