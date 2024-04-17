664eb3525e17fdb14452d86c5926289a4cb3a8d1
Gomab <egoiomab.com>
2024-04-16 20:48:05 -0700

    S4-12. Registration SeriaIIzer and View

--- api/views.py
+++ api/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from api import serializer as api_serializer 
+
+from api import serializer as api_serializer
+from userauths.models import User, Profile
+
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = api_serializer.MyTokenObtainPairSerializer
+    
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = api_serializer.RegisterSerializer
